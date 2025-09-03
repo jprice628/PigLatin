7ee8c8ae2299517d679325edbf0d0e7e6cc29931
jprice628 <jprice628@hotmail.com>
2025-09-03 17:39:29 -0400

    Token type added.

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,3 +1,11 @@
+mod tokens;
+
+use crate::tokens::Token;
+
 fn main() {
-    println!("Hello, world!");
+    let word = Token::Word("word");
+    println!("{word}");
+
+    let special = Token::Special("!");
+    println!("{special}");
 }
