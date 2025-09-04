33e08081b9b063021731d11f5fcb604007fe9388
jprice628 <jprice628@hotmail.com>
2025-09-03 23:38:32 -0400

    Pigifier added.

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,15 +1,18 @@
 mod tokens;
 mod lexer;
+mod pigifier;
 
 use crate::lexer::lex;
+use crate::pigifier::pigify;
 
 fn main() {
-    let text = r#"Hello there! This is a simple test of the Pig Latin converter. It should handle punctuation, capitalization, and even tricky words like "xylophone" or "rhythm." Don't forget contractions-those apostrophes matter. Also, consider how to treat acronyms like NASA or abbreviations such as e.g., i.e., and etc. Finally, test how it deals with numbers: 42 apples, 3.14 pies, and 100% effort!"#;
+    let text = r#"Hello there! This is a simple test of the Pig Latin converter. It should handle punctuation, capitalization, and even tricky words like "xylophone" or "rhythm." Don't forget contractions -- those apostrophes matter. Also, consider how to treat acronyms like NASA or abbreviations such as e.g., i.e., and etc. Finally, test how it deals with numbers: 42 apples, 3.14 pies, and 100% effort!"#;
 
     let mut tokens = Vec::new();
     lex(text, &mut tokens);
 
-    for token in tokens {
-        println!("{token}");
-    }
+    let mut result = String::new();
+    pigify(&tokens, &mut result);
+
+    println!("{result}");
 }
