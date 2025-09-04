cd16b7a2da773768ac19a6d9925716732459af43
jprice628 <jprice628@hotmail.com>
2025-09-03 20:31:41 -0400

    Lexer added.

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,11 +1,15 @@
 mod tokens;
+mod lexer;
 
-use crate::tokens::Token;
+use crate::lexer::lex;
 
 fn main() {
-    let word = Token::Word("word");
-    println!("{word}");
+    let text = r#"Hello there! This is a simple test of the Pig Latin converter. It should handle punctuation, capitalization, and even tricky words like "xylophone" or "rhythm." Don't forget contractions-those apostrophes matter. Also, consider how to treat acronyms like NASA or abbreviations such as e.g., i.e., and etc. Finally, test how it deals with numbers: 42 apples, 3.14 pies, and 100% effort!"#;
 
-    let special = Token::Special("!");
-    println!("{special}");
+    let mut tokens = Vec::new();
+    lex(text, &mut tokens);
+
+    for token in tokens {
+        println!("{token}");
+    }
 }
