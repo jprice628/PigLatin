4e459664f80024e4191e068ffec4474805b5d556
jprice628 <jprice628@hotmail.com>
2025-09-05 00:01:00 -0400

    Comments added to Tokens.hs.

--- Haskell/app/Tokens.hs
+++ Haskell/app/Tokens.hs
@@ -1,3 +1,5 @@
+-- Provides tokens for use as output from lexigraphical operations and functions
+-- for printing them.
 module Tokens
   ( Token (..),
     printTokens,
@@ -5,14 +7,20 @@ module Tokens
   )
 where
 
+-- Tokens for use as output from lexigraphical operations.
 data Token
+  -- Represents a word, a small string of alphabetical characters.
   = Word String
+
+  -- Represents a special character, like spaces and punctionation marks.
   | Special Char
   deriving(Show)
 
+-- Prints a list of tokens to the console.
 printTokens :: [Token] -> IO ()
 printTokens = mapM_ printToken
 
+-- Prints a single token to the console.
 printToken :: Token -> IO ()
 printToken (Word text) =
   let formattedString = "Word: " ++ text
